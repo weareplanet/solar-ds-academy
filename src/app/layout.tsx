8075628de5a569e4f0870706e1c369f0d96aec86
Felipe Castro <felipepvc@gmail.com>
2026-04-06 13:29:17 +0100

    feat: add favicon and reference in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
